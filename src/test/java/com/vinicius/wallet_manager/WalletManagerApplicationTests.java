303d397fdb1412386ffd47b684acc7d6ee1a48cc
ViniciusS4ntos <edsonklibrsouza2015@gmail.com>
2026-01-06 15:45:53 -0300

    chore: inicializacao do projeto wallet-manager

---
+++ src/test/java/com/vinicius/wallet_manager/WalletManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.wallet_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
